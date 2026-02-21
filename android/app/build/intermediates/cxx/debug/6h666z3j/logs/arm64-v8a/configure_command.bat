4553eb2970478142d230456d9d08816e15e4b597
elbriant <elbriant14@gmail.com>
2026-02-21 04:06:26 -0400

    gradle shit, idk, part 1

---
+++ android/app/build/intermediates/cxx/debug/6h666z3j/logs/arm64-v8a/configure_command.bat
@@ -0,0 +1,20 @@
+@echo off
+"C:\\Users\\Vanitarium\\AppData\\Local\\Android\\sdk\\cmake\\3.22.1\\bin\\cmake.exe" ^
+  "-HC:\\dev\\flutter\\packages\\flutter_tools\\gradle\\src\\main\\scripts" ^
+  "-DCMAKE_SYSTEM_NAME=Android" ^
+  "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON" ^
+  "-DCMAKE_SYSTEM_VERSION=24" ^
+  "-DANDROID_PLATFORM=android-24" ^
+  "-DANDROID_ABI=arm64-v8a" ^
+  "-DCMAKE_ANDROID_ARCH_ABI=arm64-v8a" ^
+  "-DANDROID_NDK=C:\\Users\\Vanitarium\\AppData\\Local\\Android\\sdk\\ndk\\27.0.12077973" ^
+  "-DCMAKE_ANDROID_NDK=C:\\Users\\Vanitarium\\AppData\\Local\\Android\\sdk\\ndk\\27.0.12077973" ^
+  "-DCMAKE_TOOLCHAIN_FILE=C:\\Users\\Vanitarium\\AppData\\Local\\Android\\sdk\\ndk\\27.0.12077973\\build\\cmake\\android.toolchain.cmake" ^
+  "-DCMAKE_MAKE_PROGRAM=C:\\Users\\Vanitarium\\AppData\\Local\\Android\\sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=D:\\dev\\sagahelper\\android\\app\\build\\intermediates\\cxx\\debug\\6h666z3j\\obj\\arm64-v8a" ^
+  "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=D:\\dev\\sagahelper\\android\\app\\build\\intermediates\\cxx\\debug\\6h666z3j\\obj\\arm64-v8a" ^
+  "-BD:\\dev\\sagahelper\\android\\app\\.cxx\\debug\\6h666z3j\\arm64-v8a" ^
+  -GNinja ^
+  -Wno-dev ^
+  --no-warn-unused-cli ^
+  "-DCMAKE_BUILD_TYPE=debug"
